a1ba462a089eb27a6f9f99e13b723ed333e74197
origin-yu <99395305+origin-yu@users.noreply.github.com>
2022-02-10 17:39:01 +0800

    Update boot.s

--- boot/x86/boot.s
+++ boot/x86/boot.s
@@ -7,31 +7,38 @@ org 7c00h
 _start:
 %define init_base 0x0500
 bits 16
-	mov ax, cs
-	mov ds, ax
-	mov ss, ax
-	; mov sp, 0
+    ; 初始化寄存器
+    mov ax, cs
+    mov ds, ax
+    mov ss, ax
+    ; mov sp, 0
+    
+    ; 屏蔽所有外部中断
     cli
+    
+    ; 清除屏幕
     call cls
-
+    
+    ; 初始化光标
     mov dx,0x0000
     call set_cursor
-
+    
+    ; 打印boot字符串
     mov si,boot
     call print_string
-
+    
+    ; 读取磁盘
     mov bx,init_base
     call load_init
-
+    
+    ; 读取成功
     mov si,load_init_success
     call print_string
 
     ;;跳转loader地址
     jmp init_base
-    
-    jmp $
 
-;;磁盘读取到内存 es:bx 地址
+; 读取磁盘内容到内存 es:bx 地址
 load_init:
     mov ah,0x02 ;读取功能
     mov al,0x0e ;读取几个扇区
@@ -45,14 +52,19 @@ load_init:
 disk_error:
     mov si,disk_erro
     call print_string
+    jmp $
 dend:
     ret
 
 %include "boot/x86/util.s"
 
+; 字符串定义(0x0a,0x0d,0表结束符)
 boot db "boot duck",0x0a,0x0d,0
 disk_erro db "read boot disk erro",0x0a,0x0d,0
 load_init_success db "load init success",0x0a,0x0d,0
 
+; 补齐至512
 times 510-($-$$)  db 0
+
+; boot结束标志
 dw 0xaa55
